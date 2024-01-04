5f6fa74715f08ca1dcda2406e0f32be83785786a
tim-hub <tim-hub@users.noreply.github.com>
2024-01-04 21:51:51 +1300

    drop unecessary filed and save as jsonl

--- converter.py
+++ converter.py
@@ -6,30 +6,35 @@ import pyarrow.orc as orc
 
 # usage: ./converter.py --help
 
-CONVERTERS = ['parquet2json',]
+CONVERTERS = ['parquet2json', ]
+
 
 def parquet2json(filename):
     df = pd.read_parquet(f'{filename}.parquet')
+    df.drop('__index_level_0__', axis=1, inplace=True)
+
+    df = df[(df['prompt'].str.len() < 12288) & (df['completion'].str.len() < 12288) & (
+                df['completion'].str.len() + df['prompt'].str.len() < 12288)]
+
     training_data = df.sample(frac=0.8, random_state=0)
-    training_data.to_json(f'{filename}.json', orient='records', lines=True)
+    training_data.to_json(f'{filename}.jsonl', orient='records', lines=True)
 
     validation_data = df.drop(training_data.index)
-    validation_data.to_json(f'{filename}_validation.json', orient='records', lines=True)
-    
+    validation_data.to_json(f'{filename}_validation.jsonl', orient='records', lines=True)
 
-    
-def convert(args): 
+
+def convert(args):
     converter = args.converter
     filename = os.path.splitext(args.sourcefile)[0]
-    
+
     switcher = {
         "parquet2json": parquet2json,
     }
-    
+
     converter_func = switcher.get(converter)
     converter_func(filename)
-    
-    
+
+
 def main():
     parser = argparse.ArgumentParser(description="Convert parquet to json | ocr to json | orc to parquet.")
     parser.add_argument("converter", choices=CONVERTERS)
@@ -37,7 +42,7 @@ def main():
     parser.set_defaults(func=convert)
     args = parser.parse_args()
     args.func(args)
-    
-    
+
+
 if __name__ == "__main__":
     main()
